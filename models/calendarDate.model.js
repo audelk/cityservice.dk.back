59324bb562b2c1a00e2eaf64015aece0d41af452
audel kabristante <audelkabristante@gmail.com>
2021-12-04 06:05:43 +0800

    update

--- models/calendarDate.model.js
+++ models/calendarDate.model.js
@@ -20,7 +20,7 @@ const calendarDateSchema = mongoose.Schema({
         type: Schema.Types.ObjectId
     },
     hours: {
-        type: [new Schema({ from: Schema.Types.Mixed, To: Schema.Types.Mixed, hour: String, available: Boolean })]
+        type: [new Schema({ from: Schema.Types.Mixed, To: Schema.Types.Mixed, available: Boolean })]
     }
 
 });
